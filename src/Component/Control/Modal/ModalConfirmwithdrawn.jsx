5956eb6e2912243205489b3e2ccdaaabafbba726
tennaja <108716351+tennaja@users.noreply.github.com>
2024-09-23 21:25:27 +0700

    Fix withdrawn

--- src/Component/Control/Modal/ModalConfirmwithdrawn.jsx
+++ src/Component/Control/Modal/ModalConfirmwithdrawn.jsx
@@ -37,6 +37,15 @@ const ModalConfirmWithdraw = (props) => {
         centered
       >
         <div className="pt-4 pb-5">
+        <div className="md:col-span-6 rounded-full rounded mx-auto flex items-center justify-center h-16 w-20  ">
+              <img
+                type="file"
+                id="preview_img"
+                className="h-20 w-20 object-cover rounded-full flex items-center justify-center"
+                src={WarningImg}
+                alt="Current profile photo"
+              />
+            </div>
           <div className="text-center sm:mt-4">
             <h6
               className="text-2xl leading-6 font-bold text-[#071437] "
